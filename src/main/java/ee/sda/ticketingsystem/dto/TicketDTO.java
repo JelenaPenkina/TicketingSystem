801fd23932b8c35ef5912561987a5a9e4b03a58b
Jelena <jelenapenkina@gmail.com>
2023-10-07 09:49:34 +0300

    Validation packages

--- src/main/java/ee/sda/ticketingsystem/dto/TicketDTO.java
+++ src/main/java/ee/sda/ticketingsystem/dto/TicketDTO.java
@@ -19,14 +19,16 @@ public class TicketDTO {
     @NotBlank(message = "Title cannot be blank")
     @Size(max = 100, message = "Title must not exceed 100 characters")
     private String title;
-
+    @NotNull(message = "Creation Date cannot be null")
     private Date creationDate;
 
     private String priority;
 
     private String category;
+
     @NotNull(message = "Status cannot be null")
     private Status status;
+
     @NotNull(message = "User cannot be null")
     private UserDTO user;
     private List<AttachmentDTO> attachments;
