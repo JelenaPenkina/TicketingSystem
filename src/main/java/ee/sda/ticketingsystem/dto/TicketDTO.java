795ef1f3cfd1e0200d0a59e19114afdb15a550be
Jelena <jelenapenkina@gmail.com>
2023-10-07 08:20:36 +0300

    Validation packages

--- src/main/java/ee/sda/ticketingsystem/dto/TicketDTO.java
+++ src/main/java/ee/sda/ticketingsystem/dto/TicketDTO.java
@@ -1,6 +1,9 @@
 package ee.sda.ticketingsystem.dto;
 
 import ee.sda.ticketingsystem.enums.Status;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -12,11 +15,19 @@ import java.util.List;
 public class TicketDTO {
 
     private Integer ticketId;
+
+    @NotBlank(message = "Title cannot be blank")
+    @Size(max = 100, message = "Title must not exceed 100 characters")
     private String title;
+
     private Date creationDate;
+
     private String priority;
+
     private String category;
+    @NotNull(message = "Status cannot be null")
     private Status status;
+    @NotNull(message = "User cannot be null")
     private UserDTO user;
     private List<AttachmentDTO> attachments;
     private List<CommentDTO> comments;
