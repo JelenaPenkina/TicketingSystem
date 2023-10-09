ad9b1864606abc7da8e9a90c29c01730df0d928c
Jelena <jelenapenkina@gmail.com>
2023-10-09 12:43:30 +0300

    Validation packages

--- src/main/java/ee/sda/ticketingsystem/dto/UserDTO.java
+++ src/main/java/ee/sda/ticketingsystem/dto/UserDTO.java
@@ -1,9 +1,7 @@
 package ee.sda.ticketingsystem.dto;
 
-
 import ee.sda.ticketingsystem.enums.UserType;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
@@ -15,9 +13,15 @@ public class UserDTO {
     private String name;
 
     @Size(max = 40, message = "Name must not exceed 40 characters")
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email can´t be empty")
     private String email;
 
     @Size(min = 8, max = 16, message = "Password must be between 8 and 16 characters")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character."
+    )
     private String password;
 
     @NotNull(message = "User type cannot be null")
